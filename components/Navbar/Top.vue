c3c3298b597cb0a7e168d6fecc9808364478eebf
Medhy Pradana Putra <masqote@gmail.com>
2022-07-16 20:25:55 +0700

    Change logo

--- components/Navbar/Top.vue
+++ components/Navbar/Top.vue
@@ -2,11 +2,16 @@
   <div>
     <!-- Navbar -->
     <div
-      class="fixed top-0 z-50 max-w-screen-sm w-full h-14 shadow-lg transition-all duration-200 bg-primary"
+      class="fixed top-0 z-50 max-w-screen-sm w-full h-14 shadow-lg transition-all duration-200 bg-white drop-shadow-lg border-b"
     >
-      <div class="flex items-center h-full px-2 space-x-2 hover:">
-        <img src="~/assets/svg/pin.svg" class="w-6 h-6" alt="" />
-        <span class="font-bold text-xl text-white">Logo</span>
+      <div class="flex items-center h-full -ml-4">
+        <img
+          src="~/assets/img/jadwal_iman_primary.png"
+          class="w-56 h-56"
+          alt=""
+          @click="$router.push('/')"
+        />
+        <!-- <span class="font-bold text-xl text-white">Logo</span> -->
       </div>
     </div>
   </div>
