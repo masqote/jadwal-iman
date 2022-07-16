40d5adb5805c190f978132465b017023645f4622
Medhy Pradana Putra <masqote@gmail.com>
2022-07-16 19:23:26 +0700

    base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://192.168.0.179:8000/api/',
+    baseURL: 'https://mamattongsis.masuk.web.id/api/',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
