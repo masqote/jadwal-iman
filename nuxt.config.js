465a315168f3580d431b2aa1b15adf3102517268
Medhy Pradana Putra <masqote@gmail.com>
2022-07-28 11:39:04 +0700

    change server

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://103.187.147.22/api/',
+    baseURL: 'https://medhypradana.my.id/api/',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
